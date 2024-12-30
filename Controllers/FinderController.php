09b821ce5604d86491f7f5ff2d10f82cc7f9a2ef
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2024-12-30 11:13:09 +0200

    work on the subscribe frontend

--- Controllers/FinderController.php
+++ Controllers/FinderController.php
@@ -1,12 +1,21 @@
 <?php
 
 require __DIR__ ."/../Models/AcceptedTrainerModel.php";
+require __DIR__ ."/../Models/User.php";
 
 class FinderController {
 public static function index(){
 $trainers = AcceptedTrainerModel::getAll();
 return $trainers;
 }
+
+public static function subscribe($trainer_id , $user_id){
+$W_trainer = AcceptedTrainerModel::getById(id: $trainer_id);
+$W_user = User::getById($user_id);
+
+
+}
+
 
 public function find(){
     
